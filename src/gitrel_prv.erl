89879a38fc3f89b6bc827a8be282cfce41737758
Daniil Sobol <sobol.daniil@gmail.com>
2016-01-22 17:35:27 +0300

    depend on tar

--- src/gitrel_prv.erl
+++ src/gitrel_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, gitrel).
--define(DEPS, []).
+-define(DEPS, [tar]).
 
 %% ===================================================================
 %% Public API
